124f787e10ccdf06ac50e0d4bf57ba9fa13481d2
Babin6139 <khatribabin39@gmail.com>
2023-02-19 22:34:38 +0545

    todo : implementing chat server using buffer and loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, 
+    io::{AsyncWriteExt,AsyncReadExt},
+};
 
 #[tokio::main]
 async fn main(){
     let listener=TcpListener::bind("localhost:8080").await.unwrap();
-    let (socket,address)=listener.accept().await.unwrap();
+    let (mut socket,_address)=listener.accept().await.unwrap();
+    loop{
+        let mut buffer=[0u8;1024];
+        let bytes_read=socket.read(&mut buffer).await.unwrap();
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
 }
