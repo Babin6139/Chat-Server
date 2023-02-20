91d5aa8a212c16cbd1ec92aad9eb253b41f21f33
Babin6139 <khatribabin39@gmail.com>
2023-02-20 23:03:36 +0545

    feat: handled multiple client connection

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
-use tokio::{net::TcpListener, 
-    io::{AsyncWriteExt,AsyncReadExt},
+use tokio::{
+    net::TcpListener, 
+    io::{AsyncWriteExt,BufReader, AsyncBufReadExt}, sync::broadcast
 };
 
 #[tokio::main]
 async fn main(){
     let listener=TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket,_address)=listener.accept().await.unwrap();
+    let (tx,rx)=broadcast::channel::<String>(10);
     loop{
-        let mut buffer=[0u8;1024];
-        let bytes_read=socket.read(&mut buffer).await.unwrap();
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        let (mut socket,_address)=listener.accept().await.unwrap();
+        let tx=tx.clone();
+        
+        tokio::spawn(async move{
+            let (reader,mut writer)=socket.split();
+        
+        let mut reader=BufReader::new(reader);
+        let mut line=String::new();
+        loop{
+            let bytes_read=reader.read_line(&mut line).await.unwrap();
+            
+            if bytes_read == 0{
+                break;
+            }
+
+            tx.send(line.clone()).unwrap();
+            
+            writer.write_all(line.as_bytes()).await.unwrap();
+            line.clear();
+
+        }
+    });
     }
 }
