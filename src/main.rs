cf9c33e3346aee1e139608dc83060c5c83e9c2c4
Babin6139 <khatribabin39@gmail.com>
2023-02-21 22:05:22 +0545

    feat: implemented chat server and completed project

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,43 @@ use tokio::{
     io::{AsyncWriteExt,BufReader, AsyncBufReadExt}, sync::broadcast
 };
 
+// fn give_me_default<T>()->T where T:Default{
+//     Default::default()
+// }
+
 #[tokio::main]
 async fn main(){
+
+    // let value=give_me_default::<i32>(); // turbofish rust
+
     let listener=TcpListener::bind("localhost:8080").await.unwrap();
-    let (tx,rx)=broadcast::channel::<String>(10);
+    let (tx,rx)=broadcast::channel(10);
     loop{
-        let (mut socket,_address)=listener.accept().await.unwrap();
+        let (mut socket,address)=listener.accept().await.unwrap();
         let tx=tx.clone();
-        
+        let mut rx=tx.subscribe();
         tokio::spawn(async move{
             let (reader,mut writer)=socket.split();
         
         let mut reader=BufReader::new(reader);
         let mut line=String::new();
         loop{
-            let bytes_read=reader.read_line(&mut line).await.unwrap();
-            
-            if bytes_read == 0{
-                break;
+            tokio::select! {
+                result = reader.read_line(&mut line)=>{
+                    if result.unwrap() == 0{
+                        break;
+                    }
+                    tx.send((line.clone(),address)).unwrap();
+                    line.clear();
+                }
+                result = rx.recv()=>{
+                    let (msg,other_addr)=result.unwrap();
+                    if address != other_addr{
+                        writer.write_all(msg.as_bytes()).await.unwrap();
+                    }
+                    
+                }
             }
-
-            tx.send(line.clone()).unwrap();
-            
-            writer.write_all(line.as_bytes()).await.unwrap();
-            line.clear();
 
         }
     });
